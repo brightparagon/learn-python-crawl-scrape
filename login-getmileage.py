190e48b35aad39c27e613ea6f62ac6fa27b28e79
노경모 <kyeongmo2@gmail.com>
2018-04-04 21:30:50 +0900

    get specific data(user related info) after login(post request)

--- login-getmileage.py
+++ login-getmileage.py
@@ -13,3 +13,18 @@ login_info = {
   "m_id": USER,
   "m_passwd": PASS
 }
+url_login = "http://www.hanbit.co.kr/member/login_proc.php"
+res = session.post(url_login, data=login_info)
+res.raise_for_status()
+
+# access to mypage
+url_mypage = "http://www.hanbit.co.kr/myhanbit/myhanbit.html"
+res = session.get(url_mypage)
+res.raise_for_status()
+
+# get mileage and ecoin
+soup = BeautifulSoup(res.text, "html.parser")
+mileage = soup.select_one(".mileage_section1 span").get_text()
+ecoin = soup.select_one(".mileage_section2 span").get_text()
+print("milage: ", mileage)
+print("ecoi: ", ecoin)
